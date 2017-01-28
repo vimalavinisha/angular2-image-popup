9ace4d8396b2bebda6f61d532dca651f4060f7b5
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-01-28 23:06:52 +0100

    rc1 with github demo updated + travisci fixed

--- travisci/travis_script.sh
+++ travisci/travis_script.sh
@@ -4,47 +4,19 @@
 echo "npm run build on $TRAVIS_OS_NAME"
 npm run build
 
+cd demo/systemjs
+npm install
+cd ../..
+cp -r dist/. demo/systemjs/node_modules/angular-modal-gallery
 
-echo "Building demo projects on $TRAVIS_OS_NAME"
-if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    npm link
-
-    cd demo/systemjs
-    npm install
-
-    cd .. # ./demo folder
-
-    cd webpack
-    npm install
-    npm link angular-modal-gallery
-    cd node_modules/angular-modal-gallery
-    rm -rf node_modules
-    cd .. # ./demo/webpack/node_modules
-    cd .. # ./demo/webpack
-    npm run build:dev
-    npm run build:prod
-    npm run build:prod:aot
-    npm run build:github:aot
-else
-    sudo npm link
-
-    cd demo/systemjs
-    npm install
-
-    cd .. # ./demo folder
-
-    cd webpack
-    npm install
-    npm link angular-modal-gallery
-    cd node_modules/angular-modal-gallery
-    rm -rf node_modules
-    cd .. # ./demo/webpack/node_modules
-    cd .. # ./demo/webpack
-    npm run build:dev
-    npm run build:prod
-    npm run build:prod:aot
-    npm run build:github:aot
-fi
-
+cd demo/webpack
+npm install
+cd ../..
+cp -r dist/. demo/webpack/node_modules/angular-modal-gallery
+cd demo/webpack
+npm run build:dev
+npm run build:prod
+npm run build:prod:aot
+npm run build:github:aot
 
 #npm run test
