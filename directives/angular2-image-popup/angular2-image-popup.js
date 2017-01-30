1a0aab7bef39a58843f0c89bd9d858cdf5d5a82e
vimala <vinisha.a@ameexusa.com>
2017-01-30 11:56:00 +0530

    angualr 2.4.4 updated

--- directives/angular2-image-popup/angular2-image-popup.js
+++ directives/angular2-image-popup/angular2-image-popup.js
@@ -1,6 +1,5 @@
-System.register(['@angular/core'], function(exports_1, context_1) {
+System.register(["@angular/core"], function (exports_1, context_1) {
     "use strict";
-    var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -10,14 +9,15 @@ System.register(['@angular/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
-    var ImageModal;
+    var __moduleName = context_1 && context_1.id;
+    var core_1, ImageModal;
     return {
-        setters:[
+        setters: [
             function (core_1_1) {
                 core_1 = core_1_1;
-            }],
-        execute: function() {
+            }
+        ],
+        execute: function () {
             ImageModal = (function () {
                 function ImageModal(element) {
                     this.element = element;
@@ -71,29 +71,29 @@ System.register(['@angular/core'], function(exports_1, context_1) {
                         }
                     }
                 };
-                __decorate([
-                    core_1.Input('modalImages'), 
-                    __metadata('design:type', Object)
-                ], ImageModal.prototype, "modalImages", void 0);
-                __decorate([
-                    core_1.Input('imagePointer'), 
-                    __metadata('design:type', Number)
-                ], ImageModal.prototype, "imagePointer", void 0);
-                __decorate([
-                    core_1.Output('cancelEvent'), 
-                    __metadata('design:type', Object)
-                ], ImageModal.prototype, "cancelEvent", void 0);
-                ImageModal = __decorate([
-                    core_1.Component({
-                        selector: 'ImageModal',
-                        template: "\n   <div class=\"ng-gallery\" *ngIf=\"showRepeat\"> \n     <div *ngFor =\"let i of modalImages; let index = index\">\n       <img src=\"{{ i.thumb }}\" class=\"ng-thumb\" (click)=\"openGallery(index)\" alt=\"Image {{ index + 1 }}\" />\n      </div>\n   </div>\n   <div class=\"ng-overlay\" *ngIf=\"opened\">\n    <div class=\"ng-gallery-content\" >\n    <div class=\"uil-ring-css\" *ngIf=\"loading\"><div></div></div>         \n    <a class=\"close-popup\" (click)=\"closeGallery()\"><i class=\"fa fa-close\"></i></a>\n     <a class=\"nav-left\" *ngIf=\"modalImages.length >1\" (click)=\"prevImage()\"><i class=\"fa fa-angle-left\"></i></a>\n     <img *ngIf=\"!loading\" src=\"{{imgSrc}}\" (click)=\"nextImage()\" class=\"effect\" />\n     <a class=\"nav-right\" *ngIf=\"modalImages.length >1\" (click)=\"nextImage()\"><i class=\"fa fa-angle-right\"></i></a>\n     <span class=\"info-text\">{{ currentImageIndex + 1 }}/{{ modalImages.length }} - Image {{currentImageIndex+1}}</span>\n   </div>\n   </div>\n       "
-                    }), 
-                    __metadata('design:paramtypes', [core_1.ElementRef])
-                ], ImageModal);
                 return ImageModal;
             }());
+            __decorate([
+                core_1.Input('modalImages'),
+                __metadata("design:type", Object)
+            ], ImageModal.prototype, "modalImages", void 0);
+            __decorate([
+                core_1.Input('imagePointer'),
+                __metadata("design:type", Number)
+            ], ImageModal.prototype, "imagePointer", void 0);
+            __decorate([
+                core_1.Output('cancelEvent'),
+                __metadata("design:type", Object)
+            ], ImageModal.prototype, "cancelEvent", void 0);
+            ImageModal = __decorate([
+                core_1.Component({
+                    selector: 'ImageModal',
+                    template: "\n   <div class=\"ng-gallery\" *ngIf=\"showRepeat\"> \n     <div *ngFor =\"let i of modalImages; let index = index\">\n       <img src=\"{{ i.thumb }}\" class=\"ng-thumb\" (click)=\"openGallery(index)\" alt=\"Image {{ index + 1 }}\" />\n      </div>\n   </div>\n   <div class=\"ng-overlay\" *ngIf=\"opened\">\n    <div class=\"ng-gallery-content\" >\n    <div class=\"uil-ring-css\" *ngIf=\"loading\"><div></div></div>         \n    <a class=\"close-popup\" (click)=\"closeGallery()\"><i class=\"fa fa-close\"></i></a>\n     <a class=\"nav-left\" *ngIf=\"modalImages.length >1\" (click)=\"prevImage()\"><i class=\"fa fa-angle-left\"></i></a>\n     <img *ngIf=\"!loading\" src=\"{{imgSrc}}\" (click)=\"nextImage()\" class=\"effect\" />\n     <a class=\"nav-right\" *ngIf=\"modalImages.length >1\" (click)=\"nextImage()\"><i class=\"fa fa-angle-right\"></i></a>\n     <span class=\"info-text\">{{ currentImageIndex + 1 }}/{{ modalImages.length }} - Image {{currentImageIndex+1}}</span>\n   </div>\n   </div>\n       "
+                }),
+                __metadata("design:paramtypes", [core_1.ElementRef])
+            ], ImageModal);
             exports_1("ImageModal", ImageModal);
         }
-    }
+    };
 });
 //# sourceMappingURL=angular2-image-popup.js.map
