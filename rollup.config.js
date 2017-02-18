f1f145828c9991a2d400a238705fd2f131e42040
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-02-18 22:09:43 +0100

    deps update + rollup externals

--- rollup.config.js
+++ rollup.config.js
@@ -51,18 +51,18 @@ export default {
       }
     })
   ],
+  external: [
+    'mousetrap',
+    'hammerjs',
+    'font-awesome',
+    '@angular/core',
+    '@angular/common',
+    'rxjs'
+  ],
   globals: {
     '@angular/core': 'ng.core',
     '@angular/common': 'ng.common',
     'rxjs/Observable': 'Rx',
-    'rxjs/ReplaySubject': 'Rx',
-    'rxjs/add/operator/map': 'Rx.Observable.prototype',
-    'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-    'rxjs/add/operator/pluck': 'Rx.Observable.prototype',
-    'rxjs/add/operator/first': 'Rx.Observable.prototype',
-    'rxjs/add/observable/fromEvent': 'Rx.Observable',
-    'rxjs/add/observable/merge': 'Rx.Observable',
-    'rxjs/add/observable/throw': 'Rx.Observable',
-    'rxjs/add/observable/of': 'Rx.Observable'
+    'rxjs/Subscription': 'Rx'
   }
 }
