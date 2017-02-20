e0924d907b1f7698c707a9fbe361aef128855d0b
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-02-20 02:28:42 +0100

    webpack demo updated

--- demo/webpack/src/app/pages/lazy/lazy.component.ts
+++ demo/webpack/src/app/pages/lazy/lazy.component.ts
@@ -22,10 +22,10 @@
  SOFTWARE.
  */
 
-import {Component, OnDestroy} from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
-import {Image, Action, ImageModalEvent} from 'angular-modal-gallery';
-import {Observable, Subscription} from "rxjs";
+import { Image, Action, ImageModalEvent, Description } from 'angular-modal-gallery';
+import { Observable, Subscription } from 'rxjs';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
 
@@ -38,67 +38,75 @@ console.log('`Lazy` component loaded asynchronously');
 })
 export class LazyComponent implements OnDestroy {
 
-  // used by example 4
   openModalWindow: boolean = false;
   imagePointer: number = 0;
 
-  // used by example 5
   openModalWindowObservable: boolean = false;
   imagePointerObservable: number = 0;
 
-  // used by both examples 2 and 4
   imagesArray = [
     new Image(
-      '../../../assets/images/gallery/thumbs/img1.jpg',
-      '../../../assets/images/gallery/img1.jpg',
-      'Image 1'
+      '../../../../assets/images/gallery/img1.jpg'
+      // no description
+      // no thumb
     ),
     new Image(
-      '../../../assets/images/gallery/thumbs/img2.jpg',
-      '../../../assets/images/gallery/img2.jpg',
-      'Image 2'
+      '../../../../assets/images/gallery/img2.jpg',
+      'Description 2'
+      // no thumb
     ),
     new Image(
-      '../../../assets/images/gallery/thumbs/img3.jpg',
-      '../../../assets/images/gallery/img3.jpg',
-      'Image 3'
+      '../../../../assets/images/gallery/img3.jpg',
+      'Description 3',
+      '../../../../assets/images/gallery/thumbs/img3.jpg'
     ),
     new Image(
-      '../../../assets/images/gallery/thumbs/img4.jpg',
-      '../../../assets/images/gallery/img4.jpg',
-      'Image 4'
+      '../../../../assets/images/gallery/img4.jpg',
+      'Description 4'
+      // no thumb
     ),
     new Image(
-      '../../../assets/images/gallery/thumbs/img5.jpg',
-      '../../../assets/images/gallery/img5.jpg',
-      'Image 5'
+      '../../../../assets/images/gallery/img5.jpg',
+      null, // no description
+      '../../../../assets/images/gallery/thumbs/img5.jpg'
     )
   ];
 
-  // used by both examples 1 and 5
   // observable of an array of images with a delay to simulate a network request
   images: Observable<Array<Image>> = Observable.of(this.imagesArray).delay(300);
 
-  // used by example 3
   // array with a single image inside (the first one)
   singleImage: Observable<Array<Image>> = Observable.of([
     new Image(
-      '../../../assets/images/gallery/thumbs/img1.jpg',
-      '../../../assets/images/gallery/img1.jpg',
-      'Image 1'
+      '../../../../assets/images/gallery/img1.jpg',
+      'Description 1',
+      '../../../../assets/images/gallery/thumbs/img1.jpg'
     )]
   );
+
+  customDescription: Description = {
+    imageText: 'Look this image ',
+    numberSeparator: ' of ',
+    beforeTextDescription: ' => '
+  };
+
+  customFullDescription: Description = {
+    // you should build this value programmaticaly with the result of (show)="..()" event
+    customFullDescription: 'Custom description of the current visible image',
+    // if customFullDescription !== undefined, all other fields will be ignored
+    // imageText: '',
+    // numberSeparator: '',
+    // beforeTextDescription: '',
+  };
 
   // rxjs subscription for example 5
   private subscription: Subscription;
 
-  // used by example 4
   openImageModal(image: Image) {
     this.imagePointer = this.imagesArray.indexOf(image);
     this.openModalWindow = true;
   }
 
-  // used by example 5
   openImageModalObservable(image: Image) {
     this.subscription = this.images.subscribe((val: Image[]) => {
       this.imagePointer = val.indexOf(image);
@@ -108,34 +116,35 @@ export class LazyComponent implements OnDestroy {
 
   onImageLoaded(event: ImageModalEvent) {
     // angular-modal-gallery will emit this event if it will load successfully input images
-    console.log("onImageLoaded action: " + Action[event.action]);
-    console.log("onImageLoaded result:" + event.result);
+    console.log('onImageLoaded action: ' + Action[event.action]);
+    console.log('onImageLoaded result:' + event.result);
   }
 
   onVisibleIndex(event: ImageModalEvent) {
-    console.log("action: " + Action[event.action]);
-    console.log("result:" + event.result);
+    this.customFullDescription.customFullDescription = `Custom description of visible image with index= ${event.result}`;
+    console.log('action: ' + Action[event.action]);
+    console.log('result:' + event.result);
   }
 
   onIsFirstImage(event: ImageModalEvent) {
-    console.log("onfirst action: " + Action[event.action]);
-    console.log("onfirst result:" + event.result);
+    console.log('onfirst action: ' + Action[event.action]);
+    console.log('onfirst result:' + event.result);
   }
 
   onIsLastImage(event: ImageModalEvent) {
-    console.log("onlast action: " + Action[event.action]);
-    console.log("onlast result:" + event.result);
+    console.log('onlast action: ' + Action[event.action]);
+    console.log('onlast result:' + event.result);
   }
 
   onCloseImageModal(event: ImageModalEvent) {
-    console.log("onClose action: " + Action[event.action]);
-    console.log("onClose result:" + event.result);
+    console.log('onClose action: ' + Action[event.action]);
+    console.log('onClose result:' + event.result);
     this.openModalWindow = false;
   }
 
   // release resources for example 5
   ngOnDestroy() {
-    if(this.subscription) {
+    if (this.subscription) {
       this.subscription.unsubscribe();
     }
   }
