d564962d2b07674a868ec6fee5fa4e0e4138236c
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-09-04 23:09:05 +0200

    fix travisci problem

--- travisci/before_script.sh
+++ travisci/before_script.sh
@@ -5,6 +5,7 @@ echo "Before script - OS is $TRAVIS_OS_NAME"
 echo "Setting xvfb based on TRAVIS_OS_NAME"
 # setting xvfb on Linux https://docs.travis-ci.com/user/gui-and-headless-browsers/#Using-xvfb-to-Run-Tests-That-Require-a-GUI
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    # config required to run browsers
     export DISPLAY=:99.0
     echo "DISPLAY is $DISPLAY"
     sh -e /etc/init.d/xvfb start
