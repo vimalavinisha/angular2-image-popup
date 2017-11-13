aa650f6e360b305f476532b32d0aa599b4181816
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2017-11-13 22:25:36 +0100

    chore: align universal example to main angular-cli

--- examples/universal-starter/src/app/home/home.component.ts
+++ examples/universal-starter/src/app/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Image } from 'angular-modal-gallery';
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Image, Action, ImageModalEvent, Description } from 'angular-modal-gallery';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+import { ButtonsConfig, ButtonsStrategy, ButtonType } from 'angular-modal-gallery';
 
 @Component({
   selector: 'home',
   templateUrl: 'home.html'
 })
 export class HomeComponent implements OnInit {
-  public message: string;
+  openModalWindow = false;
+  imagePointer = 0;
+
+  openModalWindowObservable = false;
+  imagePointerObservable = 0;
 
   imagesArray: Image[] = [
     new Image(
@@ -46,10 +57,154 @@ export class HomeComponent implements OnInit {
     )
   ];
 
-  constructor() {
-  }
+  // observable of an array of images with a delay to simulate a network request
+  images: Observable<Image[]> = Observable.of(this.imagesArray).delay(300);
+
+  // array with a single image inside (the first one)
+  singleImage: Observable<Image[]> = Observable.of([
+    new Image(
+      '../assets/images/gallery/img1.jpg',
+      '../assets/images/gallery/thumbs/img1.jpg',
+      'Description 1',
+      'http://www.google.com'
+    )]
+  );
+
+  // array of images initialized inside the onNgInit() of this component
+  // in an asynchronous way subscribing to an Observable with a delay.
+  // This is not a real use-case, but it's a way to simulate a scenario where
+  // you have to subscribe to an Observable to get data and to set public vars
+  imagesArraySubscribed: Image[];
+
+  customDescription: Description = {
+    imageText: 'Look this image ',
+    numberSeparator: ' of ',
+    beforeTextDescription: ' => '
+  };
+
+  customFullDescription: Description = {
+    // you should build this value programmaticaly with the result of (show)="..()" event
+    customFullDescription: 'Custom description of the current visible image',
+    // if customFullDescription !== undefined, all other fields will be ignored
+    // imageText: '',
+    // numberSeparator: '',
+    // beforeTextDescription: '',
+  };
+
+  // customButtonsSize: ButtonSize = {
+  //   width: 10,
+  //   height: 10,
+  //   unit: 'px'
+  // };
+
+  customButtonsConfig: ButtonsConfig = {
+    strategy: ButtonsStrategy.CUSTOM,
+    buttons: [
+      {
+        className: 'fa fa-plus white',
+        type: ButtonType.CUSTOM,
+        ariaLabel: 'custom plus aria label',
+        title: 'custom plus title',
+        fontSize: '20px'
+      },
+      {
+        className: 'fa fa-close white',
+        type: ButtonType.CLOSE,
+        ariaLabel: 'custom close aria label',
+        title: 'custom close title',
+        fontSize: '20px'
+      },
+      {
+        className: 'fa fa-refresh white',
+        type: ButtonType.REFRESH,
+        ariaLabel: 'custom refresh aria label',
+        title: 'custom refresh title',
+        fontSize: '20px'
+      },
+      {
+        className: 'fa fa-download white',
+        type: ButtonType.DOWNLOAD,
+        ariaLabel: 'custom download aria label',
+        title: 'custom download title',
+        fontSize: '20px'
+      },
+      {
+        className: 'fa fa-external-link white',
+        type: ButtonType.EXTURL,
+        ariaLabel: 'custom exturl aria label',
+        title: 'custom exturl title',
+        fontSize: '20px'
+      }
+    ]
+  };
+
+  private subscription: Subscription;
+  private imagesArraySubscription: Subscription;
 
   ngOnInit() {
-    this.message = 'Hello';
+    this.imagesArraySubscription = Observable.of(null).delay(500).subscribe(() => {
+      this.imagesArraySubscribed = this.imagesArray;
+    });
+  }
+
+  openImageModal(image: Image) {
+    this.imagePointer = this.imagesArray.indexOf(image);
+    this.openModalWindow = true;
+  }
+
+  openImageModalObservable(image: Image) {
+    this.subscription = this.images.subscribe((val: Image[]) => {
+      this.imagePointerObservable = val.indexOf(image);
+      this.openModalWindowObservable = true;
+    });
+  }
+
+  onImageLoaded(event: ImageModalEvent) {
+    // angular-modal-gallery will emit this event if it will load successfully input images
+    console.log('onImageLoaded action: ' + Action[event.action]);
+    console.log('onImageLoaded result:' + event.result);
+  }
+
+  onVisibleIndex(event: ImageModalEvent) {
+    this.customFullDescription.customFullDescription = `Custom description of visible image with index= ${event.result}`;
+    console.log('action: ' + Action[event.action]);
+    console.log('result:' + event.result);
+  }
+
+  onIsFirstImage(event: ImageModalEvent) {
+    console.log('onfirst action: ' + Action[event.action]);
+    console.log('onfirst result:' + event.result);
+  }
+
+  onIsLastImage(event: ImageModalEvent) {
+    console.log('onlast action: ' + Action[event.action]);
+    console.log('onlast result:' + event.result);
+  }
+
+  onCloseImageModal(event: ImageModalEvent) {
+    console.log('onClose action: ' + Action[event.action]);
+    console.log('onClose result:' + event.result);
+    this.openModalWindow = false;
+    this.openModalWindowObservable = false;
+  }
+
+  addRandomImage() {
+    const newImage: Image = Object.assign({},
+      this.imagesArray[Math.floor(Math.random() * this.imagesArray.length)],
+      {id: this.imagesArray.length - 1 + 1});
+    this.imagesArray.push(newImage);
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    if (this.imagesArraySubscription) {
+      this.imagesArraySubscription.unsubscribe();
+    }
+  }
+
+  trackById(index: number, item: Image) {
+    return item.id;
   }
 }
