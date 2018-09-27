d1c2260627f2e6188137aa3b3e687aac378cffe2
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2018-09-28 01:54:20 +0200

    feat(): update all examples to 7.0.0 alpha1 (systemjs is still broken) #144

---
+++ examples/systemjs/app/carousel/carousel.component.ts
@@ -0,0 +1,147 @@
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2017-2018 Stefano Cappa (Ks89)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import { Component } from '@angular/core';
+
+import { AccessibilityConfig, Image } from '@ks89/angular-modal-gallery';
+
+@Component({
+  selector: 'ks-carousel-page',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
+})
+export class CarouselComponent {
+  imageIndex = 1;
+  galleryId = 1;
+  autoPlay = true;
+  showArrows = true;
+  showDots = true;
+
+  imagesRect: Image[] = [
+    new Image(
+      0,
+      {
+        img: '../assets/images/gallery/milan-pegasus-gallery-statue.jpg',
+        description: 'Description 1'
+      },
+      { img: '../assets/images/gallery/thumbs/t-milan-pegasus-gallery-statue.jpg' }
+    ),
+    new Image(1, { img: '../assets/images/gallery/pexels-photo-47223.jpeg' }, { img: '../assets/images/gallery/thumbs/t-pexels-photo-47223.jpg' }),
+    new Image(
+      2,
+      {
+        img: '../assets/images/gallery/pexels-photo-52062.jpeg',
+        description: 'Description 3'
+      },
+      {
+        img: '../assets/images/gallery/thumbs/t-pexels-photo-52062.jpg',
+        description: 'Description 3'
+      }
+    ),
+    new Image(
+      3,
+      {
+        img: '../assets/images/gallery/pexels-photo-66943.jpeg',
+        description: 'Description 4'
+      },
+      { img: '../assets/images/gallery/thumbs/t-pexels-photo-66943.jpg' }
+    ),
+    new Image(4, { img: '../assets/images/gallery/pexels-photo-93750.jpeg' }, { img: '../assets/images/gallery/thumbs/t-pexels-photo-93750.jpg' }),
+    new Image(
+      5,
+      {
+        img: '../assets/images/gallery/pexels-photo-94420.jpeg',
+        description: 'Description 6'
+      },
+      { img: '../assets/images/gallery/thumbs/t-pexels-photo-94420.jpg' }
+    ),
+    new Image(6, { img: '../assets/images/gallery/pexels-photo-96947.jpeg' }, { img: '../assets/images/gallery/thumbs/t-pexels-photo-96947.jpg' })
+  ];
+
+  accessibilityConfig: AccessibilityConfig = {
+    backgroundAriaLabel: 'CUSTOM Modal gallery full screen background',
+    backgroundTitle: 'CUSTOM background title',
+
+    plainGalleryContentAriaLabel: 'CUSTOM Plain gallery content',
+    plainGalleryContentTitle: 'CUSTOM plain gallery content title',
+
+    modalGalleryContentAriaLabel: 'CUSTOM Modal gallery content',
+    modalGalleryContentTitle: 'CUSTOM modal gallery content title',
+
+    loadingSpinnerAriaLabel: 'CUSTOM The current image is loading. Please be patient.',
+    loadingSpinnerTitle: 'CUSTOM The current image is loading. Please be patient.',
+
+    mainContainerAriaLabel: 'CUSTOM Current image and navigation',
+    mainContainerTitle: 'CUSTOM main container title',
+    mainPrevImageAriaLabel: 'CUSTOM Previous image',
+    mainPrevImageTitle: 'CUSTOM Previous image',
+    mainNextImageAriaLabel: 'CUSTOM Next image',
+    mainNextImageTitle: 'CUSTOM Next image',
+
+    dotsContainerAriaLabel: 'CUSTOM Image navigation dots',
+    dotsContainerTitle: 'CUSTOM dots container title',
+    dotAriaLabel: 'CUSTOM Navigate to image number',
+
+    previewsContainerAriaLabel: 'CUSTOM Image previews',
+    previewsContainerTitle: 'CUSTOM previews title',
+    previewScrollPrevAriaLabel: 'CUSTOM Scroll previous previews',
+    previewScrollPrevTitle: 'CUSTOM Scroll previous previews',
+    previewScrollNextAriaLabel: 'CUSTOM Scroll next previews',
+    previewScrollNextTitle: 'CUSTOM Scroll next previews',
+
+    carouselContainerAriaLabel: 'Current image and navigation',
+    carouselContainerTitle: '',
+    carouselPrevImageAriaLabel: 'Previous image',
+    carouselPrevImageTitle: 'Previous image',
+    carouselNextImageAriaLabel: 'Next image',
+    carouselNextImageTitle: 'Next image',
+    carouselDotsContainerAriaLabel: 'Image navigation dots',
+    carouselDotsContainerTitle: '',
+    carouselDotAriaLabel: 'Navigate to image number',
+    carouselPreviewsContainerAriaLabel: 'Image previews',
+    carouselPreviewsContainerTitle: '',
+    carouselPreviewScrollPrevAriaLabel: 'Scroll previous previews',
+    carouselPreviewScrollPrevTitle: 'Scroll previous previews',
+    carouselPreviewScrollNextAriaLabel: 'Scroll next previews',
+    carouselPreviewScrollNextTitle: 'Scroll next previews'
+  };
+
+  addRandomImage() {
+    const imageToCopy: Image = this.imagesRect[Math.floor(Math.random() * this.imagesRect.length)];
+    const newImage: Image = new Image(this.imagesRect.length - 1 + 1, imageToCopy.modal, imageToCopy.plain);
+    this.imagesRect = [...this.imagesRect, newImage];
+  }
+
+  onChangeAutoPlay() {
+    this.autoPlay = !this.autoPlay;
+  }
+
+  onChangeShowArrows() {
+    this.showArrows = !this.showArrows;
+  }
+
+  onChangeShowDots() {
+    this.showDots = !this.showDots;
+  }
+}
---
+++ examples/systemjs/app/plain-gallery/plain-gallery.component.ts
@@ -0,0 +1,247 @@
+/*
+ The MIT License (MIT)
+
+ Copyright (c) 2017-2018 Stefano Cappa (Ks89)
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+import {
+  AccessibilityConfig,
+  AdvancedLayout,
+  GalleryService,
+  GridLayout,
+  Image,
+  LineLayout,
+  PlainGalleryConfig,
+  PlainGalleryStrategy
+} from '@ks89/angular-modal-gallery';
+
+@Component({
+  selector: 'ks-plain-gallery-page',
+  templateUrl: './plain-gallery.component.html',
+  styleUrls: ['./plain-gallery.component.css']
+})
+export class PlainGalleryComponent {
+  imageIndex = 1;
+  galleryId = 200;
+
+  customPlainGalleryRowConfig: PlainGalleryConfig = {
+    strategy: PlainGalleryStrategy.CUSTOM,
+    layout: new AdvancedLayout(-1, true)
+  };
+
+  customPlainGalleryColumnConfig: PlainGalleryConfig = {
+    strategy: PlainGalleryStrategy.CUSTOM,
+    layout: new AdvancedLayout(-1, true)
+  };
+
+  customPlainGalleryRowDescConfig: PlainGalleryConfig = {
+    strategy: PlainGalleryStrategy.CUSTOM,
+    layout: new AdvancedLayout(-1, true)
+  };
+
+  plainGalleryRow: PlainGalleryConfig = {
+    strategy: PlainGalleryStrategy.ROW,
+    layout: new LineLayout({ width: '80px', height: '80px' }, { length: 2, wrap: true }, 'flex-start')
+  };
+  plainGalleryRowSpaceAround: PlainGalleryConfig = {
+    strategy: PlainGalleryStrategy.ROW,
+    layout: new LineLayout({ width: '50px', height: '50px' }, { length: 2, wrap: true }, 'space-around')
+  };
+  plainGalleryRowATags: PlainGalleryConfig = {
+    strategy: PlainGalleryStrategy.ROW,
+    layout: new LineLayout({ width: '95px', height: '63px' }, { length: 4, wrap: true }, 'flex-start'),
+    // when advanced is defined, additionalBackground: '50% 50%/cover' will be used by default.
+    // I added this here, to be more explicit.
+    advanced: { aTags: true, additionalBackground: '50% 50%/cover' }
+  };
+
+  plainGalleryColumn: PlainGalleryConfig = {
+    strategy: PlainGalleryStrategy.COLUMN,
+    layout: new LineLayout({ width: '50px', height: '50px' }, { length: 3, wrap: true }, 'flex-start')
+  };
+
+  plainGalleryGrid: PlainGalleryConfig = {
+    strategy: PlainGalleryStrategy.GRID,
+    layout: new GridLayout({ width: '80px', height: '80px' }, { length: 3, wrap: true })
+  };
+
+  images: Image[] = [
+    new Image(0, {
+      img: '../assets/images/gallery/img1.jpg',
+      extUrl: 'http://www.google.com'
+    }),
+    new Image(1, {
+      img: '../assets/images/gallery/img2.jpg',
+      description: 'Description 2'
+    }),
+    new Image(
+      2,
+      {
+        img: '../assets/images/gallery/img3.jpg',
+        description: 'Description 3',
+        extUrl: 'http://www.google.com'
+      },
+      {
+        img: '../assets/images/gallery/thumbs/img3.png',
+        title: 'custom title 2',
+        alt: 'custom alt 2',
+        ariaLabel: 'arial label 2'
+      }
+    ),
+    new Image(3, {
+      img: '../assets/images/gallery/img4.jpg',
+      description: 'Description 4',
+      extUrl: 'http://www.google.com'
+    }),
+    new Image(4, { img: '../assets/images/gallery/img5.jpg' }, { img: '../assets/images/gallery/thumbs/img5.jpg' })
+  ];
+
+  imagesRect: Image[] = [
+    new Image(
+      0,
+      {
+        img: '../assets/images/gallery/milan-pegasus-gallery-statue.jpg',
+        description: 'Description 1'
+      },
+      { img: '../assets/images/gallery/thumbs/t-milan-pegasus-gallery-statue.jpg' }
+    ),
+    new Image(1, { img: '../assets/images/gallery/pexels-photo-47223.jpeg' }, { img: '../assets/images/gallery/thumbs/t-pexels-photo-47223.jpg' }),
+    new Image(
+      2,
+      {
+        img: '../assets/images/gallery/pexels-photo-52062.jpeg',
+        description: 'Description 3'
+      },
+      {
+        img: '../assets/images/gallery/thumbs/t-pexels-photo-52062.jpg',
+        description: 'Description 3'
+      }
+    ),
+    new Image(
+      3,
+      {
+        img: '../assets/images/gallery/pexels-photo-66943.jpeg',
+        description: 'Description 4'
+      },
+      { img: '../assets/images/gallery/thumbs/t-pexels-photo-66943.jpg' }
+    ),
+    new Image(4, { img: '../assets/images/gallery/pexels-photo-93750.jpeg' }, { img: '../assets/images/gallery/thumbs/t-pexels-photo-93750.jpg' }),
+    new Image(
+      5,
+      {
+        img: '../assets/images/gallery/pexels-photo-94420.jpeg',
+        description: 'Description 6'
+      },
+      { img: '../assets/images/gallery/thumbs/t-pexels-photo-94420.jpg' }
+    ),
+    new Image(6, { img: '../assets/images/gallery/pexels-photo-96947.jpeg' }, { img: '../assets/images/gallery/thumbs/t-pexels-photo-96947.jpg' })
+  ];
+
+  accessibilityConfig: AccessibilityConfig = {
+    backgroundAriaLabel: 'CUSTOM Modal gallery full screen background',
+    backgroundTitle: 'CUSTOM background title',
+
+    plainGalleryContentAriaLabel: 'CUSTOM Plain gallery content',
+    plainGalleryContentTitle: 'CUSTOM plain gallery content title',
+
+    modalGalleryContentAriaLabel: 'CUSTOM Modal gallery content',
+    modalGalleryContentTitle: 'CUSTOM modal gallery content title',
+
+    loadingSpinnerAriaLabel: 'CUSTOM The current image is loading. Please be patient.',
+    loadingSpinnerTitle: 'CUSTOM The current image is loading. Please be patient.',
+
+    mainContainerAriaLabel: 'CUSTOM Current image and navigation',
+    mainContainerTitle: 'CUSTOM main container title',
+    mainPrevImageAriaLabel: 'CUSTOM Previous image',
+    mainPrevImageTitle: 'CUSTOM Previous image',
+    mainNextImageAriaLabel: 'CUSTOM Next image',
+    mainNextImageTitle: 'CUSTOM Next image',
+
+    dotsContainerAriaLabel: 'CUSTOM Image navigation dots',
+    dotsContainerTitle: 'CUSTOM dots container title',
+    dotAriaLabel: 'CUSTOM Navigate to image number',
+
+    previewsContainerAriaLabel: 'CUSTOM Image previews',
+    previewsContainerTitle: 'CUSTOM previews title',
+    previewScrollPrevAriaLabel: 'CUSTOM Scroll previous previews',
+    previewScrollPrevTitle: 'CUSTOM Scroll previous previews',
+    previewScrollNextAriaLabel: 'CUSTOM Scroll next previews',
+    previewScrollNextTitle: 'CUSTOM Scroll next previews',
+
+    carouselContainerAriaLabel: 'Current image and navigation',
+    carouselContainerTitle: '',
+    carouselPrevImageAriaLabel: 'Previous image',
+    carouselPrevImageTitle: 'Previous image',
+    carouselNextImageAriaLabel: 'Next image',
+    carouselNextImageTitle: 'Next image',
+    carouselDotsContainerAriaLabel: 'Image navigation dots',
+    carouselDotsContainerTitle: '',
+    carouselDotAriaLabel: 'Navigate to image number',
+    carouselPreviewsContainerAriaLabel: 'Image previews',
+    carouselPreviewsContainerTitle: '',
+    carouselPreviewScrollPrevAriaLabel: 'Scroll previous previews',
+    carouselPreviewScrollPrevTitle: 'Scroll previous previews',
+    carouselPreviewScrollNextAriaLabel: 'Scroll next previews',
+    carouselPreviewScrollNextTitle: 'Scroll next previews'
+  };
+
+  constructor(private galleryService: GalleryService, private sanitizer: DomSanitizer) {}
+
+  openImageModalRow(image: Image) {
+    console.log('Opening modal gallery from custom plain gallery row, with image: ', image);
+    const index: number = this.getCurrentIndexCustomLayout(image, this.images);
+    this.customPlainGalleryRowConfig = Object.assign({}, this.customPlainGalleryRowConfig, { layout: new AdvancedLayout(index, true) });
+  }
+
+  openImageModalColumn(image: Image) {
+    console.log('Opening modal gallery from custom plain gallery column, with image: ', image);
+    const index: number = this.getCurrentIndexCustomLayout(image, this.images);
+    this.customPlainGalleryColumnConfig = Object.assign({}, this.customPlainGalleryColumnConfig, { layout: new AdvancedLayout(index, true) });
+  }
+
+  openImageModalRowDescription(image: Image) {
+    console.log('Opening modal gallery from custom plain gallery row and description, with image: ', image);
+    const index: number = this.getCurrentIndexCustomLayout(image, this.imagesRect);
+    this.customPlainGalleryRowDescConfig = Object.assign({}, this.customPlainGalleryRowDescConfig, { layout: new AdvancedLayout(index, true) });
+  }
+
+  addRandomImage() {
+    const imageToCopy: Image = this.images[Math.floor(Math.random() * this.images.length)];
+    const newImage: Image = new Image(this.images.length - 1 + 1, imageToCopy.modal, imageToCopy.plain);
+    this.images = [...this.images, newImage];
+  }
+
+  openModalViaService(id: number | undefined, index: number) {
+    console.log('opening gallery with index ' + index);
+    this.galleryService.openGallery(id, index);
+  }
+
+  trackById(index: number, item: Image) {
+    return item.id;
+  }
+
+  private getCurrentIndexCustomLayout(image: Image, images: Image[]): number {
+    return image ? images.indexOf(image) : -1;
+  }
+}
