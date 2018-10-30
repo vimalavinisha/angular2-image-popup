1aacc191de15a09fd55ac7b00d5fc8780ebfe30c
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2018-10-30 01:28:12 +0100

    refactor(): cleanup useless code of  home.component in all examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ks-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {}
