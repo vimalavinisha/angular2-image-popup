d81e9154998705973985873e9aae6db21a828fad
Stefano Cappa <stefano.cappa.ks89@gmail.com>
2021-06-10 23:44:15 +0200

    chore(): upgrade to angular 11

--- examples/universal/src/app/app-routing.module.ts
+++ examples/universal/src/app/app-routing.module.ts
@@ -18,8 +18,9 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      initialNavigation: 'enabled'
-    })
+    initialNavigation: 'enabled',
+    relativeLinkResolution: 'legacy'
+})
   ],
   exports: [RouterModule]
 })
